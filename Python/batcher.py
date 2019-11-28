07fe8f23a0c31759f5ed1696dbd050f445b7c27b
Nathan Chung <nchung77@gmail.com>
2019-11-27 17:48:40 -0800

    parse out CSV spaces

--- Python/batcher.py
+++ Python/batcher.py
@@ -10,6 +10,9 @@ import networkx as nx
 import json
 import os
 import sys
+from pathlib import Path
+
+import random
 
 import csv
 import yaml
@@ -17,6 +20,7 @@ import yaml
 loader = yaml.Loader
 
 topology = nx.Graph()
+links = []
 
 LABEL_END = "_erlang"
 
@@ -26,88 +30,45 @@ with open("../Yml/topology.yml") as file:
 
     topology.add_nodes_from(nodes)
     # for node in nodes:
-        # topology.nodes[node]["volTTL"] = 0
+    # topology.nodes[node]["volTTL"] = 0
 
     links = [key for key in list(data["links"].keys())]
 
-    topology.add_edges_from(links)
-    for link in links:
-        topology[link[0]][link[1]]["length"] = data["links"][link]["length"]
-        topology[link[0]][link[1]]["demands"] = []
+topology.add_edges_from(links)
+for link in links:
+    topology[link[0]][link[1]]["length"] = data["links"][link]["length"]
+    topology[link[0]][link[1]]["volTTL"] = 1
 
-    # adj = nx.adjacency_matrix(topology)
-    # identity = np.identity(26)
-    # a_ca = adj + identity
+# print(topology.nodes)
+# print(topology.edges)
 
-    # print(nx.normalized_laplacian_matrix(topology).A)
+# adj = nx.adjacency_matrix(topology)
+# identity = np.identity(26)
+# a_ca = adj + identity
 
-    def calc_weight(source, destination, edge):
-        source_vol_ttl = topology.nodes[source].get("volTTL")
-        destination_vol_ttl = topology.nodes[destination].get("volTTL")
-        edge_length = edge.get("weight")
-
-        return (source_vol_ttl + destination_vol_ttl) * edge_length
-
-    file_list = []
-    DATA_PATH = "../Test_Data/From_Liam/REAL-DATA-1"
-    for item in os.listdir(DATA_PATH):
-        file_list.append(item)
-
-    grouped_files = list(zip(file_list[::2], file_list[1::2]))
-
-    for item in grouped_files:
-        demand_file = item[0]
-        erlang = 300 + (grouped_files.index(item) * 100)
-        with open(os.path.join(DATA_PATH, demand_file)) as file:
-            data = json.load(file)
-            # print(data[0])
-            demands = [key[list(key.keys())[0]] for key in data]
-            net_demands = []
-            # print(demands)
-            for tick in range(555):
-                net_demands.append(demands[tick])
-                
-                for node in topology.nodes:
-                    topology.nodes[node]["volTTL"] = 0
-
-                
-                batch = []
-                lower = tick * 180
-                upper = (tick + 1) * 180
-                grouped = demands[lower:upper]
-                for demand in grouped:
-                    demand["initialttl"] -= 180 - tick
-                    if demand["initialttl"] > 0:
-                        batch.append(demand)
-                for demand in batch:
-                    erl = demand["initialttl"] * demand["volume"]
-                    if "source" in demand:
-                        shortest_path = nx.dijkstra_path(
-                            topology,
-                            demand["source"]["name"],
-                            demand["destination"]["name"],
-                            weight=calc_weight,
-                        )
-
-                        for index, node in enumerate(shortest_path)):
-                            if index != len(shortest_path):
-                                
+# print(nx.normalized_laplacian_matrix(topology).A)
 
 
-                        for node in shortest_path:
-                            topology.nodes[node]["volTTL"] += erl
+def calc_weight(source, destination, edge):
+    source_vol_ttl = topology.nodes[source].get("volTTL")
+    destination_vol_ttl = topology.nodes[destination].get("volTTL")
+    edge_length = edge.get("weight")
 
-                with open(f"data\erlang_{erlang}\edge_list_{tick}.txt", "w") as file:
-                    for node, data_dict in topology.adj.items():
-                        for nbr, length_dict in data_dict.items():
-                            data_line = " ".join(
-                                [
-                                    str(node)[5::],
-                                    str(nbr)[5::],
-                                    str(
-                                        topology.nodes[node].get("volTTL")
-                                        + topology.nodes[nbr].get("volTTL")
-                                    ),
-                                ]
-                            )
-                            file.write(f"{data_line}\n")
+    return (source_vol_ttl + destination_vol_ttl) * edge_length
+
+
+files = {}
+TEST_PATH = Path("../Test_Data/CSV-1")
+for item in TEST_PATH.iterdir():
+    if item.is_dir():
+        files[f"{item.stem}"] = []
+        for demands in item.iterdir():
+            if demands.suffix == ".csv":
+                files[f"{item.stem}"].append(demands)
+erlangs = list(files.keys())
+erlangs.append(erlangs.pop(0))
+
+# 60 * 24 * 7 = 10080 ticks per week
+# 60 ticks per timeslice
+# 168 slices per week
+
